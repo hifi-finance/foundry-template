26a66e7df130c4fc514903ca4242e1c65ec7685f
Ahmed Ihsan Tawfeeq <ahmed.i.tawfeeq@protonmail.com>
2023-02-06 12:59:35 -0700

    chore: refactor "_/install.sh"

--- _/install.sh
+++ _/install.sh
@@ -3,6 +3,6 @@ BIN_DIR="$(dirname -- "$(readlink -f -- "$0")")"
 
 git submodule update --init --recursive
 
-python $BIN_DIR/bin/pre-commit-3.0.4 install
+$BIN_DIR/bin/pre-commit-3.0.4 install
 
-python $BIN_DIR/bin/pre-commit-3.0.4 install --hook-type commit-msg
+$BIN_DIR/bin/pre-commit-3.0.4 install --hook-type commit-msg
