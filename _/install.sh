12e402df1fd1a5caa4f4a215de32e1168d6284f8
Ahmed Ihsan Tawfeeq <ahmed.i.tawfeeq@protonmail.com>
2023-02-15 13:17:37 -0700

    chore: refactor pre-commit initialization in "_/install.sh" chore: remove "_/bin" chore: refactor ".pre-commit-config.yaml"

--- _/install.sh
+++ _/install.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
-BIN_DIR="$(dirname -- "$(readlink -f -- "$0")")"
+DIR="$(dirname -- "$(readlink -f -- "$0")")"
+
+trap 'rm -rf "$DIR/tmp/"' 0
+trap 'exit $?' 1 2 3 15
 
 git submodule update --init --recursive
 
-$BIN_DIR/bin/pre-commit-3.0.4 install
+mkdir $DIR/tmp
+curl -Lso $DIR/tmp/pre-commit https://github.com/pre-commit/pre-commit/releases/download/v3.0.4/pre-commit-3.0.4.pyz
+chmod +x $DIR/tmp/pre-commit
 
-$BIN_DIR/bin/pre-commit-3.0.4 install --hook-type commit-msg
+$DIR/tmp/pre-commit install
+
+$DIR/tmp/pre-commit install --hook-type commit-msg
